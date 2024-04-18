3119eedbae6394aeeffdbbc6181991fe47170207
Johnson Giang <johnson.giang21@gmail.com>
2024-04-18 16:57:21 -0600

    dsadsa

--- components/header.js
+++ components/header.js
@@ -15,14 +15,14 @@ const Header = () => {
 
     return (
         <>
-            <div className="flex items-center justify-between bg-gray-800 text-white shadow-lg w-full px-4 py-2">
+            <div className="flex items-center justify-between bg-orange-800 text-white shadow-lg w-full px-4 py-2">
                 <div className="flex items-center space-x-4">
                     <img src="/NBA logo.jpg" alt="NBA Logo" className="h-12 w-auto" />
                     <h1 className="text-3xl font-bold text-white">NBA Player App</h1>
                 </div>
                 <ul className="flex justify-center gap-10">
                     {navItems.map((link, index) => (
-                        <li key={index} className={`font-medium text-lg ${router.pathname === link.href ? "text-blue-500" : "text-gray-300 hover:text-white"}`}>
+                        <li key={index} className={`font-medium text-lg ${router.pathname === link.href ? "text-yellow-500" : "text-orange-300 hover:text-white"}`}>
                             <Link href={link.href}>{link.label}</Link>
                         </li>
                     ))}
