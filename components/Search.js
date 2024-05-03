c43edf90a9b887df4aa479e11674c1bd3151d439
Johnson Giang <johnson.giang21@gmail.com>
2024-05-03 01:52:41 -0600

    dasd

--- components/Search.js
+++ components/Search.js
@@ -37,7 +37,7 @@ const PlayersList = ({ player = [] }) => {
             </button>
             <ul>
                 {filteredPlayers.map(p => (
-                    <SearchPlayer key={p.id} player={p} />
+                    <li key={p.id} player={p} />
                 ))}
             </ul>
         </div>
