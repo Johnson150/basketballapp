fabcf79376060454ade361e9b23462d77d64d923
Johnson Giang <johnson.giang21@gmail.com>
2024-05-03 02:12:39 -0600

    dasda

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,13 @@ import NextAuth from "next-auth";
 import authConfig from "@/app/libs/auth";
 
 //handle all nextauth routes
+export default NextAuth({
+
+    providers: [
+
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
+})
 const handler = NextAuth(authConfig);
 
-export { handler as GET, handler as POST};
+export { handler as GET, handler as POST };
